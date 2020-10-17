d68789eca78ab2e8abbcb4e91408669df80fe471
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-10-17 12:43:45 +0900

    create post html

--- myblog/blogs/models.py
+++ myblog/blogs/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Article(models.Model):
-    titile=models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
     text = models.TextField()
     posted_at = models.DateTimeField(auto_now_add=True)
     last_modify = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
-        return self.titile
+        return self.title
--- myblog/blogs/views.py
+++ myblog/blogs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Article
+
 
 def index(request):
     template_name = "blogs/index.html"
+    return render(request, template_name)
+
+
+def new(request):
+    template_name = "blogs/new.html"
+    if request.method == "POST":
+        Article.objects.create(title=request.POST["title"], text=request.POST["text"])
     return render(request, template_name)
