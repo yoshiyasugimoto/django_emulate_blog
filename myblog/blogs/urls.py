4d54e033bf33faef4215028984feec69f675425a
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-10-17 17:29:59 +0900

    Create edit page

--- myblog/blogs/urls.py
+++ myblog/blogs/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("new/", views.new, name="new"),
     path("article/all/", views.article_all, name="article_all"),
-    re_path(r"article/(\d+)/", views.view_article, name="view_article"),
+    re_path(r"article/([0-9]+)/", views.view_article, name="view_article"),
+    re_path(r"article/([0-9]+)/edit/", views.edit_article, name="edit_article")
 ]
